f032eac0621ea83e1ebb821d5cac298f6a2003e5
Vanderal Talagi <vanderaltalagi@gmail.com>
2022-07-13 17:32:46 +1200

    commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-  </React.StrictMode>
-);
+class Navbar extends React.Component {
+  render() {
+    return (
+      <nav className='nav-bar'>
+        <ul>
+          <li id="home">Home</li>
+          <li id="testimonials">Testimonials</li>
+          <li id="plan">Plan</li>
+          <li id="contact">Contact</li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+class Page extends React.Component {
+  render(){
+    return(
+      <Navbar />
+    )
+  }
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// ===============================================================>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Page />);
