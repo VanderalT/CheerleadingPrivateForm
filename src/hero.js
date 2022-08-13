d78043ce3578ed648e279764c6968b0260733b67
Vanderal Talagi <vanderaltalagi@gmail.com>
2022-08-13 15:22:03 +1200

    Added Hero-Section with placeholders No interactivity

---
+++ src/hero.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './index.scss';
+
+class Hero extends React.Component {
+    render(){
+        return(
+            <div className='hero-section'>
+                <div className='hero-photos'>
+                    <div className='placeholder'/>
+                </div>
+                <div className='header-group'>
+                    <div className='headers'>
+                        <h1>Coach Van</h1>
+                        <h2>Private Tumble Lessons</h2>
+                        <div className='book-now-button'>
+                            <h2>BOOK NOW!</h2>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Hero
--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import Hero from './hero';
 import './index.scss';
 
+/*
+TO DO:
+Minimum viable product
+-Hero + Header
+-Testimonials
+-Levels
+-Booking Form//
+*/
+
+class Page extends React.Component {
+  render(){
+    return(
+      <div className='website-container'>
+        <Hero/>
+      </div>
+    )
+  }
+}
 
 // ===============================================================>
 
