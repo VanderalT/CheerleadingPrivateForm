d0a445ff64b5796376d0d4be12db5f339f5347ed
Vanderal Talagi <vanderaltalagi@gmail.com>
2022-08-15 20:48:42 +1200

    Added styling Added content to Levels container

--- src/levels/level-three.js
+++ src/levels/level-three.js
@@ -5,6 +5,18 @@ class LevelThree extends React.Component {
         return(
             <div className='level-three-container'>
                 <h1>Level Three</h1>
+                <p>
+                    Once level one and two skills are consistent and producing power, we can start learning to connect skills together and start free flipping skills!
+                    <br></br>If an athlete is already at this level, I will work with them to break down the skills into their individual parts to focus on what needs the most work.
+                    <br></br>
+                    <br></br>To quote Shane Montgomery, INTENSITY™ Master Trainer at CheerConditioning.Academy and one of the judges at the last global games: 
+                    <br></br><a href='https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Ftalktoshanemontgomery%2Fposts%2Fpfbid027pSsArg385CzpXVjJZrob9USCgbdvo4wpjzyr7zt5dLcLvLCrmHFz9p3PtabXfXdl&show_text=true&width=500' target='_blank'>'The hardest part of the roundoff handspring double Is the roundoff'</a>.
+                </p>
+                <h1>Skills</h1>
+                <ul>
+                    <li>Standing + Running multiple handsprings</li>
+                    <li>Free flipping skills -e.g back tucks-</li>
+                </ul>
             </div>
         )
     }
